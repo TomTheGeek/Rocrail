3f312ee2cabcc44fab0fd279fc0476f6bd04b7c9
Mark Edwards <mrex@interport.net>
2009-03-27 21:16:02 -0400

    Commented, check for running server fixed to work with new shell script name

--- rocrail/package/rocrail.sh
+++ rocrail/package/rocrail.sh
@@ -1,37 +1,55 @@
 #!/bin/sh
+#
+#    Copyright 2009 Rocrail.net.  See www.rocrail.net for license details
+#
 
-if [ ! -e ~/rocrail ] ; then
-	mkdir ~/rocrail
+# Check if we are running
+
+if [ "`ps a | grep 'rocrail -console' | grep -v 'grep'`" != "" ] ; then
+  echo "Rocrail is already running."
+  exit
+else
+  echo "Starting Rocrail..."
 fi
 
+#
+# Set program locations
+#
+
 if [ "`which rocprops.sh`" != "" ] ; then
-	source rocprops.sh
+
+#       Use locations from make install
+
+	. rocprops.sh
 else
-	# Use default property values
+
+#       Use default locations
+
 	ROCRAIL_LIBDIR=/usr/local/lib/rocrail
 	ROCRAIL_LIBEXECDIR=/usr/local/libexec/rocrail
 	ROCRAIL_DATADIR=/usr/local/share/rocrail
 fi
 
+#
+# Create user directory and copy default ini and plan files
+#
+
+if [ ! -e ~/rocrail ] ; then
+	mkdir ~/rocrail
+fi
 if [ ! -e ~/rocrail/rocrail.ini ] ; then
 	cp $ROCRAIL_DATADIR/default/rocrail.ini ~/rocrail
 fi
-
 if [ ! -e ~/rocrail/plan.xml ] ; then
 	cp $ROCRAIL_DATADIR/default/plan.xml ~/rocrail
 fi
-
 if [ ! -e ~/rocrail/neustadt.xml ] ; then
 	cp $ROCRAIL_DATADIR/default/neustadt.xml ~/rocrail
 fi
 
-cd ~/rocrail
+# Run Rocrail
 
-if ps --no-heading -C rocrail
-  then
-    echo "rocrail is running"
-  else
-    echo "rocrail is not running. start..."
-#    gnome-terminal --hide-menubar --title=Rocrail --working-directory="~/rocrail" --geometry=132x50 -e "/opt/rocrail/rocrail -console -l /opt/rocrail"
-    $ROCRAIL_LIBEXECDIR/rocrail -console -l $ROCRAIL_LIBDIR
-fi
+cd ~/rocrail
+echo "Starting Rocrail..."
+$ROCRAIL_LIBEXECDIR/rocrail -console -l $ROCRAIL_LIBDIR
+
