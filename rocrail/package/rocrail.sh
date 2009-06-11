bff9acf5d9f282d6481c3f25580701325c7cdc2d
rob <>
2009-06-11 20:05:11 +0200

    revert makefiles and scripts

--- rocrail/package/rocrail.sh
+++ rocrail/package/rocrail.sh
@@ -1,28 +1,12 @@
 #!/bin/sh
-
 if [ ! -e ~/rocrail ] ; then
 	mkdir ~/rocrail
 fi
 
-if [ "`which rocprops.sh`" != "" ] ; then
-	source rocprops.sh
-else
-	# Use default property values
-	ROCRAIL_LIBDIR=/usr/local/lib/rocrail
-	ROCRAIL_LIBEXECDIR=/usr/local/libexec/rocrail
-	ROCRAIL_DATADIR=/usr/local/share/rocrail
-fi
-
 if [ ! -e ~/rocrail/rocrail.ini ] ; then
-	cp $ROCRAIL_DATADIR/default/rocrail.ini ~/rocrail
-fi
-
-if [ ! -e ~/rocrail/plan.xml ] ; then
-	cp $ROCRAIL_DATADIR/default/plan.xml ~/rocrail
-fi
-
-if [ ! -e ~/rocrail/neustadt.xml ] ; then
-	cp $ROCRAIL_DATADIR/default/neustadt.xml ~/rocrail
+	cp /opt/rocrail/default/rocrail.ini ~/rocrail
+	cp /opt/rocrail/default/plan.xml ~/rocrail
+	cp /opt/rocrail/default/neustadt.xml ~/rocrail
 fi
 
 cd ~/rocrail
@@ -33,5 +17,6 @@ if ps --no-heading -C rocrail
   else
     echo "rocrail is not running. start..."
 #    gnome-terminal --hide-menubar --title=Rocrail --working-directory="~/rocrail" --geometry=132x50 -e "/opt/rocrail/rocrail -console -l /opt/rocrail"
-    $ROCRAIL_LIBEXECDIR/rocrail -console -l $ROCRAIL_LIBDIR
+    /opt/rocrail/rocrail -console -l /opt/rocrail
 fi
+
--- rocrail/package/rocview.sh
+++ rocrail/package/rocview.sh
@@ -1,60 +1,26 @@
 #!/bin/sh
-#
-#    Copyright 2009 Rocrail.net.  See www.rocrail.net for license details
-#
-
-#
-# Set program locations
-#
-
-if [ "`which rocprops.sh`" != "" ] ; then
-	. rocprops.sh
-else
-
-        # Use default property values
-
-	ROCRAIL_LIBEXECDIR=/usr/local/libexec/rocrail
-	ROCRAIL_DATADIR=/usr/local/share/rocrail
-fi
-
-#
-# Create user directory and copy default ini and plan files
-#
-
 if [ ! -e ~/rocrail ] ; then
 	mkdir ~/rocrail
 fi
+
 if [ ! -e ~/rocrail/rocgui.ini ] ; then
-	cp $ROCRAIL_DATADIR/default/rocgui.ini ~/rocrail
+	cp /opt/rocrail/default/rocgui.ini ~/rocrail
+	cp /opt/rocrail/default/plan.xml ~/rocrail
 fi
-if [ ! -e ~/rocrail/plan.xml ] ; then
-	cp $ROCRAIL_DATADIR/default/plan.xml ~/rocrail
-fi
-if [ ! -e ~/rocrail/images ] ; then
-	cp -Rp $ROCRAIL_DATADIR/images ~/rocrail
-fi
-
-#
-# Link icons
-#
-
-if [ ! -e ~/rocrail/icons ] ; then
-	ln -s $ROCRAIL_DATADIR/icons ~/rocrail/icons
-fi
-
-#
-# Overwrite themes, preserving user directories
-#
-
-if [ ! -e ~/rocrail/svg ] ; then
-	mkdir -p ~/rocrail/svg
-fi
-cp -Rp $ROCRAIL_DATADIR/svg/* ~/rocrail/svg
-
-#
-# Run Rocview
-#
 
 cd ~/rocrail
-$ROCRAIL_LIBEXECDIR/rocview $*
+
+if [ ! -e ~/rocrail/icons ] ; then
+	ln -s /opt/rocrail/icons ~/rocrail/icons
+fi
+
+if [ ! -e ~/rocrail/svg ] ; then
+	ln -s /opt/rocrail/svg ~/rocrail/svg
+fi
+
+if [ ! -e ~/rocrail/images ] ; then
+	cp -r /opt/rocrail/images ~/rocrail/images
+fi
+
+/opt/rocrail/rocview $1 $2 $3
 
