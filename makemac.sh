cc1573f423136785dda7ef18f81f94602cdfef0c
Jean-Michel Fischer <jmf@macbook.local>
2009-07-11 09:58:08 +0200

    makemac.sh - pac and build a dmg

---
+++ makemac.sh
@@ -0,0 +1,96 @@
+#!/bin/sh
+#
+# pac-mac.sh -- Copyright 2009 Rocrail.net.  See www.rocrail.net for license details
+#
+echo ""
+echo "*** Rocrail makemac.sh starting (see www.rocrail.net)..."
+echo ""
+
+# Check params
+# rocrail-setup-[version].[patch]-rev[revno]-[type]-[dist].exe  (md5)
+
+VERSION=$1
+PATCH=$2
+TYPE=$3
+DIST=$4
+
+echo "Checking Parameters..."
+
+if [ !  $1 ] || [ ! $2 ] || [ ! $3 ] || [ ! $4 ]; then
+  echo "Error: Missing parameters:"
+  echo ""
+  echo "    Usage: makemac.sh <version> <patch> <type> <dist>"
+  echo ""
+  echo "    Example: \"makemac.sh 1.2 999 snapshot x64\" will build "
+  echo "    \"rocrail-1.2.999-revXXX-snapshot-x64.dmg\" where \"XXX\" is "
+  echo "    the Bazaar revision number or \"user\" if Bazaar is not installed."
+  echo ""
+  exit $?
+else
+  echo "    Parameters okay"
+  echo ""
+fi
+
+# create the .apps
+cd rocrail; make macapp; cd ..;
+cd rocgui; make macapp; cd ..;
+
+echo "Getting Bazaar revision number..."
+if which bzr > /dev/null
+then
+	BAZAARREV=`bzr revno`
+	echo "    Revision number is $BAZAARREV"
+	echo ""
+else
+	BAZAARREV="user"
+	echo "    Revision number not found or Bazaar not installed, using \"user\""
+	echo ""
+fi
+
+echo "Building rocrail-$VERSION.$PATCH-rev$BAZAARREV-$TYPE-$DIST.dmg"
+echo ""
+
+# Create an initial disk image (200 megs)
+hdiutil create -size 200m -fs HFS+ -volname "Rocrail" myimg.dmg
+
+# Mount the disk image
+hdiutil attach myimg.dmg
+
+# Obtain device information
+DEVS=$(hdiutil attach myimg.dmg | cut -f 1)
+DEV=$(echo $DEVS | cut -f 1 -d ' ')
+
+DEST=$(hdiutil attach myimg.dmg | cut -f 3 )
+
+# copy the files
+mkdir -p $DEST/Rocrail/rocdata/
+
+cp -r unxbin/Rocrail.app $DEST/Rocrail
+cp -r unxbin/Rocview.app $DEST/Rocrail
+
+cp rocrail/package/plan.xml $DEST/Rocrail/rocdata
+cp -r rocgui/svg $DEST/Rocrail/rocdata/
+
+# pimp the dmg
+cp doc/rocrail-logo-r.png $DEST/background.png
+setFile -a V $DEST/background.png
+cp rocrail/rocrail.icns $DEST/VolumeIcon.icns
+setFile -a V $DEST/VolumeIcon.icns
+cp rocgui/_DS_Store $DEST/.DS_Store
+setFile -a V $DEST/.DS_Store
+
+# Unmount the disk image
+hdiutil detach $DEV
+
+# Convert the disk image to read-only
+hdiutil convert myimg.dmg -format UDZO -o rocrail.dmg
+
+#clean up
+rm -f myimg.dmg
+
+if [ ! -e package ] ; then
+	echo "    package not found, creating it"
+	mkdir package
+fi
+
+mv rocrail.dmg package/rocrail-$VERSION.$PATCH-rev$BAZAARREV-$TYPE-$DIST.dmg
