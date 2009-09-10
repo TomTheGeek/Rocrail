3ddfead2c4106c12053169aaf02daa998afb2468
Jean-Michel Fischer <jmf@talin>
2009-09-10 20:54:14 +0200

    sync clock message in opendcc to debug

--- rocdigs/impl/opendcc.c
+++ rocdigs/impl/opendcc.c
@@ -135,7 +135,7 @@ static iONode _cmd( obj inst ,const iONode cmd ) {
     int wday    = lTime->tm_wday;
     int divider = wClock.getdivider(cmd);
 
-    TraceOp.trc( name, TRCLEVEL_INFO, __LINE__, 9999,
+    TraceOp.trc( name, TRCLEVEL_DEBUG, __LINE__, 9999,
         "%s clock to %02d:%02d divider=%d", wClock.getcmd( cmd ), hours, mins, divider );
 
     if( StrOp.equals( wDigInt.p50x, data->sublibname )  && StrOp.equals( wClock.set, wClock.getcmd( cmd ) ) ) {
