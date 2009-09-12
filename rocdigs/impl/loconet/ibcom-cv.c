93900869ee4df256ef2893da73158348846c2760
r.j.versluis@rocrail.net <>
2009-09-12 09:10:25 +0200

    422214 reverse engineering of IB-Com programming

--- rocdigs/impl/loconet/ibcom-cv.c
+++ rocdigs/impl/loconet/ibcom-cv.c
@@ -13,7 +13,7 @@ IB -> PC: B4 3B 00 70                                     OPC_LONG_ACK, on OPC_R
 PC -> IB: E5 07 01 49 42 41 56                            OPC_PEER_XFER, src=7, dst=9345, ??
 PC -> IB: 82 7D                                           OPC_GPOFF
 
-# read cv 1                    CV CV
+# read cv 1                  R CV CV
 PC -> IB: ED 1F 01 49 42 71 72 01 00 00 70 00 00 00 00 10 OPC_IMM_PACKET
           00 00 00 00 00 00 00 00 00 00 00 00 00 00 65
 IB -> PC: B4 6D 01 27                                     OPC_LONG_ACK, on OPC_IMM_PACKET
@@ -28,7 +28,7 @@ PC -> IB: E5 07 01 49 42 40 57                            OPC_PEER_XFER, src=7, 
 PC -> IB: E5 07 01 49 42 41 56                            OPC_PEER_XFER, src=7, dst=
 PC -> IB: 82 7D                                           OPC_GPOFF
 
-# write cv 1                   CV CV VV
+# write cv 1                 W CV CV VV
 PC -> IB: ED 1F 01 49 42 71 71 01 00 03 70 00 00 00 00 10 OPC_IMM_PACKET
           00 00 00 00 00 00 00 00 00 00 00 00 00 00 65
 IB -> PC: B4 6D 01 27                                     OPC_LONG_ACK, on OPC_IMM_PACKET
@@ -38,6 +38,26 @@ IB -> PC: E7 0E 7C 00 00 00 71 06 00 00 03 00 00 1E       OPC_SL_RD_DATA, len, P
 # end off programming session
 PC -> IB: E5 07 01 49 42 40 57                            OPC_PEER_XFER, src=7, dst=
 
+
+
+# write 254 in cv 27
+               HB  W CV CV VV
+ED 1F 01 49 42 79 71 1B 00 7E 70 00 00 00 00 10
+00 00 00 00 00 00 00 00 00 00 00 00 00 00 0A
+# HB = high bit for CV value
+
+# response
+                        HB    VV
+E7 0E 7C 00 00 00 71 06 02 00 7E 00 00 61
+
 */
 
+int makeIBComCVPacket(int cv, byte* buffer) {
+  return 0;
+}
+
+
+int evaluateIBComCVPacket(byte* buffer) {
+  return 0;
+}
 
---
+++ rocdigs/impl/loconet/ibcom-cv.h
@@ -0,0 +1,14 @@
+/*
+ * ibcom-cv.h
+ *
+ *  Created on: Sep 12, 2009
+ *      Author: rob
+ */
+
+#ifndef IBCOMCV_H_
+#define IBCOMCV_H_
+
+int makeIBComCVPacket(int cv, byte* buffer);
+int evaluateIBComCVPacket(byte* buffer);
+
+#endif /* IBCOMCV_H_ */
