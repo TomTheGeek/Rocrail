8a0b3581ef632466d82cf966bf448fb0ead90f8f
r.j.versluis@rocrail.net <>
2009-11-29 10:29:22 +0100

    added the rocnet library to the list

--- rocview/dialogs/rocrailinidialog.cpp
+++ rocview/dialogs/rocrailinidialog.cpp
@@ -438,6 +438,7 @@ void RocrailIniDialog::initValues() {
   m_Lib->Append( wxString( wDigInt.p50, wxConvUTF8 ) );
   m_Lib->Append( wxString( wDigInt.p50x, wxConvUTF8 ) );
   m_Lib->Append( wxString( wDigInt.railcom, wxConvUTF8 ) );
+  m_Lib->Append( wxString( wDigInt.rocnet, wxConvUTF8 ) );
   m_Lib->Append( wxString( wDigInt.roco, wxConvUTF8 ) );
   m_Lib->Append( wxString( wDigInt.slx, wxConvUTF8 ) );
   m_Lib->Append( wxString( wDigInt.sprog, wxConvUTF8 ) );
