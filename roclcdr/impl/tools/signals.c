ca05c61ac23fd62c030c51983f09357a396ee474
r.j.versluis@rocrail.net <>
2010-01-10 08:42:55 +0100

    regard thrown switches for signals in case of 'secondnext'

--- roclcdr/impl/tools/signals.c
+++ roclcdr/impl/tools/signals.c
@@ -261,8 +261,17 @@ Boolean setSignals(iOLcDriver inst, Boolean onEnter ) {
       data->next2Block != data->next3Block )
   {
     if( data->greenaspect ) {
-      data->next2Block->green( data->next2Block, True, !data->next3RouteFromTo );
-      data->next2Block->green( data->next2Block, False, !data->next3RouteFromTo );
+      if( data->next3Route->hasThrownSwitch(data->next3Route) ) {
+        TraceOp.trc( name, TRCLEVEL_USER1, __LINE__, 9999,
+                     "setting signals for next2Block to white: thrown switches in route [%s]",
+                     data->next3Route->getId(data->next3Route) );
+        data->next2Block->white( data->next2Block, True, !data->next3RouteFromTo );
+        data->next2Block->white( data->next2Block, False, !data->next3RouteFromTo );
+      }
+      else {
+        data->next2Block->green( data->next2Block, True, !data->next3RouteFromTo );
+        data->next2Block->green( data->next2Block, False, !data->next3RouteFromTo );
+      }
     }
     else {
       data->next2Block->yellow( data->next2Block, True, !data->next3RouteFromTo );
