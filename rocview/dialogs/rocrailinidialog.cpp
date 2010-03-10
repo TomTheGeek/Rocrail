27aa59a468fc8f412d4180254fc9ab75ee300564
r.j.versluis@rocrail.net <>
2010-03-10 20:32:21 +0100

    disabled the lenz library in favour of xpressnet

--- rocview/dialogs/rocrailinidialog.cpp
+++ rocview/dialogs/rocrailinidialog.cpp
@@ -434,7 +434,7 @@ void RocrailIniDialog::initValues() {
   m_Lib->Append( wxString( wDigInt.hsi88, wxConvUTF8 ) );
   m_Lib->Append( wxString( wDigInt.inter10, wxConvUTF8 ) );
   m_Lib->Append( wxString( wDigInt.io8255, wxConvUTF8 ) );
-  m_Lib->Append( wxString( wDigInt.lenz, wxConvUTF8 ) );
+  //m_Lib->Append( wxString( wDigInt.lenz, wxConvUTF8 ) );
   m_Lib->Append( wxString( wDigInt.loconet, wxConvUTF8 ) );
   m_Lib->Append( wxString( wDigInt.lrc135, wxConvUTF8 ) );
   m_Lib->Append( wxString( wDigInt.massoth, wxConvUTF8 ) );
