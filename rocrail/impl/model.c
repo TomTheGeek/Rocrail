7769b02fcc49f9a5523a11d2e32be8869d7102e1
rob <>
2010-08-20 08:20:41 +0200

    fix for setting the donation flag in the plan for the client

--- rocrail/impl/model.c
+++ rocrail/impl/model.c
@@ -1678,13 +1678,15 @@ static Boolean _cmd( iOModel inst, iONode cmd ) {
     char* version = StrOp.fmt( "%d.%d.%d-%d", wGlobal.vmajor, wGlobal.vminor, wGlobal.patch, AppOp.getrevno() );
     wPlan.setrocrailversion( data->model, version );
 
-    if( SystemOp.isExpired(SystemOp.decode(StrOp.strToByte(wRocRail.getdonkey(AppOp.getIni())),
+    if( !SystemOp.isExpired(SystemOp.decode(StrOp.strToByte(wRocRail.getdonkey(AppOp.getIni())),
         StrOp.len(wRocRail.getdonkey(AppOp.getIni()))/2, wRocRail.getdoneml(AppOp.getIni())), NULL) ) {
       wPlan.setdonkey(data->model, True);
+      TraceOp.trc( name, TRCLEVEL_INFO, __LINE__, 9999, "valid donation key: %s", wRocRail.getdoneml(AppOp.getIni()) );
     }
-    else
+    else {
       wPlan.setdonkey(data->model, False);
-
+      TraceOp.trc( name, TRCLEVEL_INFO, __LINE__, 9999, "no valid donation key found" );
+    }
 
     wAutoCmd.setcmd( autoevent, ModelOp.isAuto(inst)?wAutoCmd.on:wAutoCmd.off );
     wState.setconsolemode( stateevent, AppOp.isConsoleMode() );
