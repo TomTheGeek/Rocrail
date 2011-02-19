9c9783f5a92262f5357a2f42a2190884d5beacf2
r.j.versluis@rocrail.net <>
2011-02-19 20:44:23 +0100

    command line variable added in the unix rocrail.sh

--- rocrail/package/rocrail.sh
+++ rocrail/package/rocrail.sh
@@ -11,6 +11,6 @@ if ps --no-heading -C rocrail
   else
     echo "rocrail is not running. start..."
 #    gnome-terminal --hide-menubar --title=Rocrail --working-directory="~/rocrail" --geometry=132x50 -e "/opt/rocrail/rocrail -console -l /opt/rocrail"
-    /opt/rocrail/rocrail -console -l /opt/rocrail
+    /opt/rocrail/rocrail -console -l /opt/rocrail $1 $2 $3 $4
 fi
 
