36791bcdcd0c01ddfea16f04d137a40cc4e5c6ff
rob <>
2011-08-30 19:18:51 +0200

    cbus: device number mapped on address instead of port for switches

--- rocrail/impl/model.c
+++ rocrail/impl/model.c
@@ -2844,8 +2844,8 @@ static void _event( iOModel inst, iONode nodeC ) {
     else {
       NodeOp.setName(nodeC, wSwitch.name());
       wSwitch.setbus( nodeC, bus );
-      wSwitch.setaddr1( nodeC, 0 );
-      wSwitch.setport1( nodeC, addr );
+      wSwitch.setaddr1( nodeC, addr );
+      wSwitch.setport1( nodeC, 0 );
       wSwitch.setstate( nodeC, val?"straight":"turnout" );
     }
 
