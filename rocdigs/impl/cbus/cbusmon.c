55fbafdd75e3423d20fd0532b642d9ee120cbe9c
rob <>
2011-09-25 14:54:00 +0200

    cbus monitor added: (WIP)

--- rocdigs/impl/cbus/cbusmon.c
+++ rocdigs/impl/cbus/cbusmon.c
@@ -31,12 +31,54 @@ void cbusMon(byte* frame, int opc) {
   int offset  = (frame[1] == 'S') ? 0:4;
   int addrh   = 0;
   int addrl   = 0;
+  int session = 0;
+  int speed   = 0;
+  int flags   = 0;
+  int steps   = 128;
+  int f0      = 0;
+  int f1      = 0;
+  int f2      = 0;
 
   switch(opc) {
+  case OPC_RESTP:
+    TraceOp.trc( name, TRCLEVEL_MONITOR, __LINE__, 9999, "OPC_RESTP(0x%02X) request emergency stop all", opc );
+    break;
+
   case OPC_RLOC:
     addrh   = HEXA2Byte(frame + OFFSET_D1 + offset) & 0x3F;
     addrl   = HEXA2Byte(frame + OFFSET_D2 + offset);
     TraceOp.trc( name, TRCLEVEL_MONITOR, __LINE__, 9999, "OPC_RLOC(0x%02X) request loco [%d] assignment", opc, addrl+addrh*256 );
+    break;
+
+  case OPC_PLOC:
+    session = HEXA2Byte(frame + OFFSET_D1 + offset);
+    addrh   = HEXA2Byte(frame + OFFSET_D2 + offset) & 0x3F;
+    addrl   = HEXA2Byte(frame + OFFSET_D3 + offset);
+    speed   = HEXA2Byte(frame + OFFSET_D4 + offset);
+    f0      = HEXA2Byte(frame + OFFSET_D5 + offset);
+    f1      = HEXA2Byte(frame + OFFSET_D6 + offset);
+    f2      = HEXA2Byte(frame + OFFSET_D7 + offset);
+    TraceOp.trc( name, TRCLEVEL_MONITOR, __LINE__, 9999,
+        "OPC_PLOC(0x%02X) loco [%d] report: session=%d speed=%d dir=%s f0-4=0x%02X f5-8=0x%02X f9-12=0x%02X",
+        opc, addrl+addrh*256, session, speed&0x7F, (speed&0x80)?"fwd":"rev", f0, f1, f2 );
+    break;
+
+  case OPC_DSPD:
+    session = HEXA2Byte(frame + OFFSET_D1 + offset);
+    speed   = HEXA2Byte(frame + OFFSET_D2 + offset);
+    TraceOp.trc( name, TRCLEVEL_MONITOR, __LINE__, 9999,
+        "OPC_DSPD(0x%02X) loco speed/dir: session=%d speed=%d dir=%s",
+        opc, session, speed&0x7F, (speed&0x80)?"fwd":"rev" );
+    break;
+
+  case OPC_DFLG:
+    session = HEXA2Byte(frame + OFFSET_D1 + offset);
+    flags   = HEXA2Byte(frame + OFFSET_D2 + offset);
+    if( flags & 0x02 ) steps = 28;
+    else if( flags & 0x01 ) steps = 14;
+    TraceOp.trc( name, TRCLEVEL_MONITOR, __LINE__, 9999,
+        "OPC_DFLG(0x%02X) loco flags: session=%d flags=0x%02X steps=%d lights=%s state=%d",
+        opc, session, flags, steps, (flags&0x04)?"on":"off", (flags&0x30)>>4 );
     break;
 
   default:
