2200ffa3f6627a5328c2ddeb1aa0fd0363c53f8a
rob <>
2011-11-06 15:45:44 +0100

    editspro: v1.0 wip

--- rocdigs/impl/editspro.c
+++ rocdigs/impl/editspro.c
@@ -44,6 +44,23 @@
 
 #include "rocutils/public/addr.h"
 
+
+#define INITBYTE 0x37
+#define FORWARD 0
+#define REVERSE 1
+#define LOCCTRLBYTE 0x08
+#define ACCCTRLBYTE 0x0C
+#define ACCOUTPUT1 0x00
+#define ACCOUTPUT2 0x03
+#define ACCOUTPUT3 0x0C
+#define ACCOUTPUT4 0x0F
+#define ACCOUTPUT5 0x30
+#define ACCOUTPUT6 0x33
+#define ACCOUTPUT7 0x3C
+#define ACCOUTPUT8 0x3F
+
+
+
 static int instCnt = 0;
 
 /** ----- OBase ----- */
@@ -437,6 +454,67 @@ static Boolean __flush( iOEditsProData data ) {
 
 
 
+__initAddrMap(iOEditsPro  edits) {
+  iOEditsProData data = Data(edits);
+  data->addr[ 0] = 0x00; data->addr[10] = 0x33; data->addr[20] = 0x11;
+  data->addr[ 1] = 0x03; data->addr[11] = 0x31; data->addr[21] = 0x1C;
+  data->addr[ 2] = 0x01; data->addr[12] = 0x3C; data->addr[22] = 0x1F;
+  data->addr[ 3] = 0xC0; data->addr[13] = 0x3F; data->addr[23] = 0x1D;
+  data->addr[ 4] = 0x0F; data->addr[14] = 0x3D; data->addr[24] = 0x14;
+  data->addr[ 5] = 0x0D; data->addr[15] = 0x34; data->addr[25] = 0x17;
+  data->addr[ 6] = 0x04; data->addr[16] = 0x37; data->addr[26] = 0x15;
+  data->addr[ 7] = 0x07; data->addr[17] = 0x35; data->addr[27] = 0xC0;
+  data->addr[ 8] = 0x05; data->addr[18] = 0x10; data->addr[28] = 0xC2;
+  data->addr[ 9] = 0x30; data->addr[19] = 0x13; data->addr[29] = 0xC1;
+
+  data->addr[30] = 0xCC; data->addr[40] = 0xFF; data->addr[50] = 0xDD;
+  data->addr[31] = 0xCF; data->addr[41] = 0xFC; data->addr[51] = 0xD4;
+  data->addr[32] = 0xCD; data->addr[42] = 0xF4; data->addr[52] = 0xD7;
+  data->addr[33] = 0xC4; data->addr[43] = 0xF7; data->addr[53] = 0xD5;
+  data->addr[34] = 0xC7; data->addr[44] = 0xF5; data->addr[54] = 0x40;
+  data->addr[35] = 0xC5; data->addr[45] = 0xD0; data->addr[55] = 0x43;
+  data->addr[36] = 0xF0; data->addr[46] = 0xD3; data->addr[56] = 0x41;
+  data->addr[37] = 0xF3; data->addr[47] = 0xD1; data->addr[57] = 0x4C;
+  data->addr[38] = 0xF1; data->addr[48] = 0xDC; data->addr[58] = 0x4F;
+  data->addr[39] = 0xFC; data->addr[49] = 0xDF; data->addr[59] = 0x4D;
+
+  data->addr[60] = 0x44; data->addr[70] = 0x77; data->addr[80] = 0x55;
+  data->addr[61] = 0x47; data->addr[71] = 0x75;
+  data->addr[62] = 0x45; data->addr[72] = 0x50;
+  data->addr[63] = 0x70; data->addr[73] = 0x53;
+  data->addr[64] = 0x73; data->addr[74] = 0x51;
+  data->addr[65] = 0x71; data->addr[75] = 0x5C;
+  data->addr[66] = 0x7C; data->addr[76] = 0x5F;
+  data->addr[67] = 0x7F; data->addr[77] = 0x5D;
+  data->addr[68] = 0x7D; data->addr[78] = 0x54;
+  data->addr[69] = 0x74; data->addr[79] = 0x57;
+
+  data->speed[FORWARD][ 0] = 136; data->speed[REVERSE][ 0] = 162;
+  data->speed[FORWARD][ 1] = 137; data->speed[REVERSE][ 1] = 163;
+  data->speed[FORWARD][ 2] = 140; data->speed[REVERSE][ 2] = 166;
+  data->speed[FORWARD][ 3] = 141; data->speed[REVERSE][ 3] = 167;
+  data->speed[FORWARD][ 4] = 152; data->speed[REVERSE][ 4] = 178;
+  data->speed[FORWARD][ 5] = 153; data->speed[REVERSE][ 5] = 179;
+  data->speed[FORWARD][ 6] = 156; data->speed[REVERSE][ 6] = 182;
+  data->speed[FORWARD][ 7] = 157; data->speed[REVERSE][ 7] = 183;
+  data->speed[FORWARD][ 8] =  72; data->speed[REVERSE][ 8] =  98;
+  data->speed[FORWARD][ 9] =  73; data->speed[REVERSE][ 9] =  99;
+  data->speed[FORWARD][10] =  76; data->speed[REVERSE][10] = 102;
+  data->speed[FORWARD][11] =  77; data->speed[REVERSE][11] = 103;
+  data->speed[FORWARD][12] =  88; data->speed[REVERSE][12] = 114;
+  data->speed[FORWARD][13] =  89; data->speed[REVERSE][13] = 115;
+  data->speed[FORWARD][14] =  92; data->speed[REVERSE][14] = 118;
+  data->speed[FORWARD][15] =  93; data->speed[REVERSE][15] = 119;
+
+  data->accgate[0][0] = ACCOUTPUT1;
+  data->accgate[0][1] = ACCOUTPUT2;
+  data->accgate[1][0] = ACCOUTPUT3;
+  data->accgate[1][1] = ACCOUTPUT4;
+  data->accgate[2][0] = ACCOUTPUT5;
+  data->accgate[2][1] = ACCOUTPUT6;
+  data->accgate[3][0] = ACCOUTPUT7;
+  data->accgate[3][1] = ACCOUTPUT8;
+}
 
 
 
@@ -450,18 +528,21 @@ static struct OEditsPro* _inst( const iONode ini ,const iOTrace trc ) {
   SystemOp.inst();
 
   /* Initialize data->xxx members... */
+  data->ini      = ini;
   data->device   = StrOp.dup( wDigInt.getdevice( ini ) );
   data->iid      = StrOp.dup( wDigInt.getiid( ini ) );
 
-  data->fbmod    = wDigInt.getfbmod( ini );
-  data->swtime   = wDigInt.getswtime( ini );
-  data->readfb   = wDigInt.isreadfb( ini );
+  data->fbmod     = wDigInt.getfbmod( ini );
+  data->swtime    = wDigInt.getswtime( ini );
+  data->readfb    = wDigInt.isreadfb( ini );
   data->lastSwCmd = -1;
+  data->v10       = wDigInt.getprotver(ini) == 10 ? True:False;
 
   TraceOp.trc( name, TRCLEVEL_INFO, __LINE__, 9999, "----------------------------------------" );
-  TraceOp.trc( name, TRCLEVEL_INFO, __LINE__, 9999, "EDiTS PRO v1.2 %d.%d.%d", vmajor, vminor, patch );
+  TraceOp.trc( name, TRCLEVEL_INFO, __LINE__, 9999, "EDiTS PRO %d.%d.%d", vmajor, vminor, patch );
   TraceOp.trc( name, TRCLEVEL_INFO, __LINE__, 9999, "----------------------------------------" );
   TraceOp.trc( name, TRCLEVEL_INFO, __LINE__, 9999, "iid       = %s", data->iid );
+  TraceOp.trc( name, TRCLEVEL_INFO, __LINE__, 9999, "version   = %s", data->v10 ? "1.0":"1.2" );
   TraceOp.trc( name, TRCLEVEL_INFO, __LINE__, 9999, "device    = %s", data->device );
   TraceOp.trc( name, TRCLEVEL_INFO, __LINE__, 9999, "baudrate  = %d", 9600 );
   TraceOp.trc( name, TRCLEVEL_INFO, __LINE__, 9999, "handshake = %s", wDigInt.getflow(ini) );
@@ -469,6 +550,10 @@ static struct OEditsPro* _inst( const iONode ini ,const iOTrace trc ) {
   TraceOp.trc( name, TRCLEVEL_INFO, __LINE__, 9999, "fbmodules = %d", wDigInt.getfbmod(ini) );
   TraceOp.trc( name, TRCLEVEL_INFO, __LINE__, 9999, "read fb   = %s", wDigInt.isreadfb(ini)?"yes":"no" );
   TraceOp.trc( name, TRCLEVEL_INFO, __LINE__, 9999, "----------------------------------------" );
+
+  if(data->v10) {
+    __initAddrMap(__EditsPro);
+  }
 
   data->serial = SerialOp.inst( data->device );
   SerialOp.setFlow( data->serial, StrOp.equals( wDigInt.cts, wDigInt.getflow( data->ini ) ) ? cts:none );
