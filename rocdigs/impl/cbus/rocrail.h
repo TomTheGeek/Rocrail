86fd95cd989deff33a1c8ec8be9f77600d1d91fa
r.j.versluis@rocrail.net <>
2011-12-15 09:01:03 +0100

    cbus: cangc6 added

--- rocdigs/impl/cbus/rocrail.h
+++ rocdigs/impl/cbus/rocrail.h
@@ -5,6 +5,7 @@
 #define MANU_ROCRAIL 70
 // Module types
 #define MTYP_CANGC2  2 // 16 I/O
+#define MTYP_CANGC6  6 // 4 Chanal Servo
 #define MTYP_CANGC7  7 // Fast clock
 
 #define OPC_PNN    0xB6    // <NN hi><NN lo><manu id><module id><flags>
