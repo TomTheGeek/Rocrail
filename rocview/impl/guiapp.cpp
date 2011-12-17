8859841ee07ec5cc7795751150590ff0b0acf02a
r.j.versluis@rocrail.net <>
2011-12-17 08:02:01 +0100

    wx2.9.2 workaround for window height size

--- rocview/impl/guiapp.cpp
+++ rocview/impl/guiapp.cpp
@@ -214,7 +214,7 @@ int RocGui::OnExit() {
     wWindow.setcy( wGui.getwindow( m_Ini ), size.GetHeight() );
 
 #ifdef __APPLE__
-    wWindow.setcy( wGui.getwindow( m_Ini ), size.GetHeight() - 37 ); // hard coded work around for increasing height.
+    wWindow.setcy( wGui.getwindow( m_Ini ), size.GetHeight() - 32 ); // hard coded work around for increasing height.
 #endif
 
     if( wGui.getrrcon( m_Ini ) == NULL ) {
