dc628985b9ec791a44b997796d2b0f00684ae7ac
r.j.versluis@rocrail.net <>
2012-04-26 20:43:47 +0200

    copyright added

--- rocview/impl/clock.cpp
+++ rocview/impl/clock.cpp
@@ -1,10 +1,21 @@
-/**
-* Name:    wxAnalogClock.h
-* Purpose:     Analog clock
-* Author:      emarti, Murat Özdemir  e-mail: dtemarti<add>gmail<dot>com
-* Created:     15.04.2006
-* Copyright:   (c) emarti
-* Licence:     wxWindows license
+/*
+ Rocrail - Model Railroad Software
+
+ Copyright (C) 2002-2012 - Rob Versluis <r.j.versluis@rocrail.net>
+
+ This program is free software; you can redistribute it and/or
+ modify it under the terms of the GNU General Public License
+ as published by the Free Software Foundation; either version 2
+ of the License, or (at your option) any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with this program; if not, write to the Free Software
+ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 */
 
 // ----------------------------------------------------------------------------
