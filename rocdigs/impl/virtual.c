5e0eb356093075ccdcf5d68a6cf5e919586561f4
r.j.versluis@rocrail.net <>
2012-07-09 17:28:27 +0200

    virtual: show changed function number

--- rocdigs/impl/virtual.c
+++ rocdigs/impl/virtual.c
@@ -310,8 +310,8 @@ static iONode __translate( iOVirtual virtual, iONode node ) {
     Boolean f11 = wFunCmd.isf11( node );
     Boolean f12 = wFunCmd.isf12( node );
     TraceOp.trc( name, TRCLEVEL_MONITOR, __LINE__, 9999,
-            "decoder %d f0=%s f1=%s f2=%s f3=%s f4=%s f5=%s f6=%s f7=%s f8=%s f9=%s f10=%s f11=%s f12=%s",
-            addr, f0?"on":"off", f1?"on":"off", f2?"on":"off", f3?"on":"off", f4?"on":"off",
+            "decoder %d fc=%d f0=%s f1=%s f2=%s f3=%s f4=%s f5=%s f6=%s f7=%s f8=%s f9=%s f10=%s f11=%s f12=%s",
+            addr, wFunCmd.getfnchanged(node) , f0?"on":"off", f1?"on":"off", f2?"on":"off", f3?"on":"off", f4?"on":"off",
             f5?"on":"off", f6?"on":"off", f7?"on":"off", f8?"on":"off",
             f9?"on":"off", f10?"on":"off", f11?"on":"off", f12?"on":"off" );
   }
