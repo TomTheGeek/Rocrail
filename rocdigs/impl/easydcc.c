9fa61c66ebde851cee37fb13f341ab3e6abfbf79
r.j.versluis@rocrail.net <>
2012-08-21 16:52:46 +0200

    easydcc: added loco speed, loco functions, switches and outputs

--- rocdigs/impl/easydcc.c
+++ rocdigs/impl/easydcc.c
@@ -25,6 +25,7 @@
 
 
 #include "rocdigs/impl/easydcc_impl.h"
+#include "rocdigs/impl/nmra/nmrapacket.h"
 
 #include "rocs/public/mem.h"
 #include "rocs/public/system.h"
@@ -98,6 +99,20 @@ static void* __event( void* inst, const void* evt ) {
 
 /** ----- OEasyDCC ----- */
 
+static void __reportState(iOEasyDCCData data) {
+  if( data->listenerFun != NULL && data->listenerObj != NULL ) {
+    iONode node = NodeOp.inst( wState.name(), NULL, ELEMENT_NODE );
+
+    if( data->iid != NULL )
+      wState.setiid( node, data->iid );
+    wState.setpower( node, data->power );
+
+    data->listenerFun( data->listenerObj, node, TRCLEVEL_INFO );
+  }
+}
+
+
+
 static Boolean __sendCommand(iOEasyDCCData data, char* cmd) {
   Boolean rc = False;
   TraceOp.trc( name, TRCLEVEL_BYTE, __LINE__, 9999, "send command: %s", cmd );
@@ -107,27 +122,160 @@ static Boolean __sendCommand(iOEasyDCCData data, char* cmd) {
   return rc;
 }
 
+
+static int __normalizeSteps(int insteps ) {
+  /* SPEEDSTEPS: vaild: 14, 28, 128 */
+  if( insteps < 20 )
+    return 14;
+  if( insteps > 100 )
+    return 128;
+  return 28;
+}
+
+static void __makeMessage(char* buffer, char* cmd, byte* retVal, int len) {
+  char* s;
+  int idx = 0;
+  int i = 0;
+
+  for( i = 0; i < StrOp.len(cmd); i++) {
+    buffer[idx] = cmd[i];
+    idx++;
+  }
+
+  TraceOp.dump( name, TRCLEVEL_BYTE, (char*)retVal, len );
+
+  for( i = 0; i < len; i++ ) {
+    buffer[idx] = ' ';
+    idx++;
+    s = StrOp.byteToStr( &retVal[i], 1);
+    buffer[idx] = s[0];
+    idx++;
+    buffer[idx] = s[1];
+    idx++;
+    StrOp.free(s);
+  }
+  buffer[idx] = '\r';
+}
+
+
 static iONode __translate( iOEasyDCCData data, iONode node ) {
-  iONode reply = NULL;
+  iONode rsp = NULL;
   const char* cmd = wSysCmd.getcmd( node );
   char buffer[256];
 
   TraceOp.trc( name, TRCLEVEL_MONITOR, __LINE__, 9999, "translate command: %s:%s", NodeOp.getName(node), cmd );
 
+  /* System command. */
   if( StrOp.equals( NodeOp.getName( node ), wSysCmd.name() ) ) {
     if( StrOp.equals( cmd, wSysCmd.stop ) || StrOp.equals( cmd, wSysCmd.ebreak ) ) {
       StrOp.fmtb( buffer, "K\r" );
       TraceOp.trc( name, TRCLEVEL_MONITOR, __LINE__, 9999, "Kill main track" );
       __sendCommand(data, buffer);
+      data->power = False;
+      __reportState(data);
     }
     else if( StrOp.equals( cmd, wSysCmd.go ) ) {
       StrOp.fmtb( buffer, "E\r" );
       TraceOp.trc( name, TRCLEVEL_MONITOR, __LINE__, 9999, "Enable main track" );
       __sendCommand(data, buffer);
+      data->power = True;
+      __reportState(data);
     }
   }
 
-  return reply;
+  /* Switch command. */
+  else if( StrOp.equals( NodeOp.getName( node ), wSwitch.name() ) ) {
+    int addr = wSwitch.getaddr1(node);
+    int outputChannel = StrOp.equals( wSwitch.turnout, wSwitch.getcmd(node)) ? 1:0;
+    int len = 0;
+    byte retVal[32];
+    len = accDecoderPkt2(retVal, addr, True, outputChannel);
+
+    __makeMessage(buffer, "S 02", retVal, len);
+
+    TraceOp.trc( name, TRCLEVEL_MONITOR, __LINE__, 9999, "turnout %d %s", addr, wSwitch.getcmd(node) );
+    __sendCommand(data, buffer);
+  }
+
+  /* Output command. */
+  else if( StrOp.equals( NodeOp.getName( node ), wOutput.name() ) ) {
+    int addr = wOutput.getaddr(node);
+    int outputChannel = wOutput.getgate(node);
+    int len = 0;
+    byte retVal[32];
+    len = accDecoderPkt2(retVal, addr, StrOp.equals(wOutput.on, wOutput.getcmd(node)), outputChannel);
+
+    __makeMessage(buffer, "S 02", retVal, len);
+
+    TraceOp.trc( name, TRCLEVEL_MONITOR, __LINE__, 9999, "output %d %s", addr, wSwitch.getcmd(node) );
+    __sendCommand(data, buffer);
+  }
+
+  /* Sensor command. */
+  else if( StrOp.equals( NodeOp.getName( node ), wFeedback.name() ) ) {
+    int addr = wFeedback.getaddr( node );
+    Boolean state = wFeedback.isstate( node );
+
+    TraceOp.trc( name, TRCLEVEL_MONITOR, __LINE__, 9999, "simulate fb addr=%d state=%s", addr, state?"true":"false" );
+    rsp = (iONode)NodeOp.base.clone( node );
+  }
+
+  /* Loc command. */
+  else if( StrOp.equals( NodeOp.getName( node ), wLoc.name() ) ) {
+    int     steps = __normalizeSteps( wLoc.getspcnt(node) );
+    int     addr  = wLoc.getaddr(node);
+    Boolean fn    = wLoc.isfn( node );
+    Boolean longAddr = StrOp.equals( wLoc.getprot( node ), wLoc.prot_L );
+    int     dir   = wLoc.isdir( node );
+
+    int len = 0;
+    byte retVal[32];
+    int  speed = 0;
+
+    if( wLoc.getV( node ) != -1 ) {
+      if( StrOp.equals( wLoc.getV_mode( node ), wLoc.V_mode_percent ) )
+        speed = (wLoc.getV( node ) * steps) / 100;
+      else if( wLoc.getV_max( node ) > 0 )
+        speed = (wLoc.getV( node ) * steps) / wLoc.getV_max( node );
+    }
+    TraceOp.trc( name, TRCLEVEL_MONITOR, __LINE__, 9999, "loco %d speed=%d lights=%s dir=%s",
+        addr, speed, fn?"on":"off", dir?"forwards":"reverse" );
+
+    if( steps == 128 )
+      len = speedStep128Packet( retVal, addr, longAddr, speed, dir );
+    else if( steps == 28 )
+      len = speedStep28Packet( retVal, addr, longAddr, speed, dir );
+    else
+      len = speedStep14Packet( retVal, addr, longAddr, speed, dir, fn );
+
+    __makeMessage(buffer, "Q", retVal, len);
+
+    __sendCommand(data, buffer);
+
+  }
+
+  /* Function command. */
+  else if( StrOp.equals( NodeOp.getName( node ), wFunCmd.name() ) ) {
+    int addr  = wLoc.getaddr(node);
+    Boolean longAddr = StrOp.equals( wLoc.getprot( node ), wLoc.prot_L );
+    int fg = wFunCmd.getgroup(node);
+    int len = 0;
+    byte retVal[32];
+
+    if( fg == 0 || fg == 1 )
+      len = function0Through4Packet( retVal, addr, longAddr, wFunCmd.isf0(node), wFunCmd.isf1(node), wFunCmd.isf2(node), wFunCmd.isf3(node), wFunCmd.isf4(node) );
+
+    __makeMessage(buffer, "S 01", retVal, len);
+
+    __sendCommand(data, buffer);
+  }
+
+  /* Program command. */
+  else if( StrOp.equals( NodeOp.getName( node ), wProgram.name() ) ) {
+  }
+
+
+  return rsp;
 }
 
 /**  */
@@ -160,7 +308,8 @@ static void _halt( obj inst ,Boolean poweroff ) {
   data->run = False;
   TraceOp.trc( name, TRCLEVEL_INFO, __LINE__, 9999, "shutting down <%s>...", data->iid );
   ThreadOp.sleep(100);
-  SerialOp.close( data->serial );
+  if( data->serial != NULL )
+    SerialOp.close( data->serial );
 }
 
 
