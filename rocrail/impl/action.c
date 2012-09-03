b3d9d4df64f9041ab18655d724a2b20e97480585
ron&bram <ronaldestherbram@telfort.nl>
2012-09-03 10:54:05 +0200

    Action: sensor counter reset use resetcounter, not empty command

--- rocrail/impl/action.c
+++ rocrail/impl/action.c
@@ -467,14 +467,14 @@ static void __executeAction( struct OAction* inst, iONode actionctrl ) {
       const char* cmdStr = wAction.getcmd( data->action );
       int error = 0;
       wFeedback.setid( cmd, id );
-      if( !StrOp.equals("", cmdStr) ) {
-        wFeedback.setstate( cmd, StrOp.equals("on", cmdStr) ? True:False);
-        TraceOp.trc( name, TRCLEVEL_INFO, __LINE__, 9999, "setting sensor [%s] to [%s]", id, cmdStr );
-        FBackOp.event( fb, cmd );
-      } else {
+      if( StrOp.equals("resetcounter", cmdStr) ) {
         wFeedback.setcmd( cmd, wFeedback.reset );
         TraceOp.trc( name, TRCLEVEL_INFO, __LINE__, 9999, "resetting counter sensor [%s]", id );
         FBackOp.cmd( fb, cmd, True );
+      } else {
+        wFeedback.setstate( cmd, StrOp.equals("on", cmdStr) ? True:False);
+        TraceOp.trc( name, TRCLEVEL_INFO, __LINE__, 9999, "setting sensor [%s] to [%s]", id, cmdStr );
+        FBackOp.event( fb, cmd );
       }
     }
   }
