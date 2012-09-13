b1bb97334de61e4165d7cf979ca4db00db77cfe9
r.j.versluis@rocrail.net <>
2012-09-13 16:03:27 +0200

    rocrail startup script: optional gnome terminal

--- rocrail/package/rocrail.sh
+++ rocrail/package/rocrail.sh
@@ -2,7 +2,10 @@
 if [ ! -e ~/rocrail ] ; then
 	mkdir ~/rocrail
 fi
-gnome_terminal=`command -v gnome-terminal`
+
+if [ ! $NO_GNOME_PLEASE ] ; then
+  gnome_terminal=`command -v gnome-terminal`
+fi
 
 cd ~/rocrail
 
@@ -14,7 +17,7 @@ if ps --no-heading -C rocrail
     
     if [ $gnome_terminal ]
       then
-        $gnome_terminal --hide-menubar --title=Rocrail --geometry=132x50 -e "/opt/rocrail/rocrail -console -l /opt/rocrail"
+        $gnome_terminal --hide-menubar --title=Rocrail --geometry=132x50 -e "/opt/rocrail/rocrail -console -l /opt/rocrailn $*" 
       else
         /opt/rocrail/rocrail -console -l /opt/rocrail $*
     fi
