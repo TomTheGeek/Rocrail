3281b7fc2af296a3ea01bf0d56d1f612fd0fc89e
r.j.versluis@rocrail.net <>
2012-12-03 20:33:05 +0100

    virtual: trace out the ID of the link/ulink command

--- rocdigs/impl/virtual.c
+++ rocdigs/impl/virtual.c
@@ -349,10 +349,12 @@ static iONode __translate( iOVirtual virtual, iONode node ) {
       TraceOp.trc( name, TRCLEVEL_MONITOR, __LINE__, 9999, "send short IDs to the throttle" );
     }
     else if( StrOp.equals( cmd, wSysCmd.link ) ) {
-      TraceOp.trc( name, TRCLEVEL_MONITOR, __LINE__, 9999, "link TD port %d to %d",  wSysCmd.getvalA(node),  wSysCmd.getvalB(node) );
+      TraceOp.trc( name, TRCLEVEL_MONITOR, __LINE__, 9999, "link %s TD port %d to %d",
+          wSysCmd.getid(node),  wSysCmd.getvalA(node),  wSysCmd.getvalB(node) );
     }
     else if( StrOp.equals( cmd, wSysCmd.ulink ) ) {
-      TraceOp.trc( name, TRCLEVEL_MONITOR, __LINE__, 9999, "unlink TD port %d",  wSysCmd.getvalA(node) );
+      TraceOp.trc( name, TRCLEVEL_MONITOR, __LINE__, 9999, "unlink %s TD port %d",
+        wSysCmd.getid(node),  wSysCmd.getvalA(node),  wSysCmd.getvalB(node) );
     }
     else {
       TraceOp.trc( name, TRCLEVEL_MONITOR, __LINE__, 9999, "syscmd=%s", cmd );
