f41b56023809040c854cb037350a970e33fccabf
r.j.versluis@rocrail.net <>
2013-01-05 19:53:02 +0100

    trace correction for processing block enterside on create

--- rocview/impl/item.cpp
+++ rocview/impl/item.cpp
@@ -2676,7 +2676,7 @@ void Symbol::modelEvent( iONode node, bool oncreate ) {
             }
           }
         }
-        else {
+        else if(!oncreate) {
           TraceOp.trc( "item", TRCLEVEL_WARNING, __LINE__, 9999, "not possible to process enterside; locoid not set" );
         }
       }
