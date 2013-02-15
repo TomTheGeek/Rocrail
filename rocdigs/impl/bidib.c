2b2936c3661e1b939f4934074f311b35ee0ff3e4
r.j.versluis@rocrail.net <>
2013-02-15 10:02:10 +0100

    bidib: only report occupied on address > 0 to prevent ghost

--- rocdigs/impl/bidib.c
+++ rocdigs/impl/bidib.c
@@ -1824,14 +1824,18 @@ static Boolean __processBidiMsg(iOBiDiB bidib, byte* msg, int size) {
    */
   case MSG_BM_OCC:
   { // len = 4
-    bidibnode->occ[pdata[0]] = True;
-    __handleSensor(bidib, bidibnode->uid, pdata[0], True, 0, -1);
+    TraceOp.trc( name, TRCLEVEL_EXCEPTION, __LINE__, 9999,"BM port %d occupied", pdata[0]);
+    if(!bidibnode->occ[pdata[0]]) {
+      bidibnode->occ[pdata[0]] = True;
+      __handleSensor(bidib, bidibnode->uid, pdata[0], True, 0, -1);
+    }
     __seqAck(bidib, bidibnode, MSG_BM_MIRROR_OCC, pdata, datasize);
     break;
   }
 
   case MSG_BM_FREE:
   { // len = 4
+    TraceOp.trc( name, TRCLEVEL_EXCEPTION, __LINE__, 9999,"BM port %d free", pdata[0]);
     bidibnode->occ[pdata[0]] = False;
     __handleSensor(bidib, bidibnode->uid, pdata[0], False, 0, -1);
     __seqAck(bidib, bidibnode, MSG_BM_MIRROR_FREE, pdata, datasize);
@@ -1865,9 +1869,10 @@ static Boolean __processBidiMsg(iOBiDiB bidib, byte* msg, int size) {
     }
     else {
       if( locoAddr > 0 ) {
+        TraceOp.trc( name, TRCLEVEL_EXCEPTION, __LINE__, 9999,"BM port %d reports loco %d", port, locoAddr);
         bidibnode->occ[port] = True;
+        __handleSensor(bidib, bidibnode->uid, pdata[0], bidibnode->occ[port], locoAddr, type );
       }
-      __handleSensor(bidib, bidibnode->uid, pdata[0], bidibnode->occ[port], locoAddr, type );
     }
     break;
   }
