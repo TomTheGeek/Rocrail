4c98d0530da2508ddabf8dea57fc92be6b8c317c
rob <>
2013-03-06 09:31:36 +0100

    massoth: overrule throttle option implemented

--- rocdigs/impl/massoth.c
+++ rocdigs/impl/massoth.c
@@ -306,7 +306,7 @@ static iOSlot __getSlot(iOMassothData data, iONode node) {
   cmd[1] = 0; /*xor*/
   cmd[2] = addr >> 8;
   cmd[3] = addr & 0x00FF;
-  cmd[4] = 0x90;
+  cmd[4] = 0x90 + (wDigInt.isoverrule(data->ini)?0x40:0x00);
 
   if( __transact( data, cmd, rsp, 0x40, &gotid ) ) {
     if( gotid ) {
