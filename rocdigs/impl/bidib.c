ef7bffd79831bf7ef31cf13a1a589430162f9cea
r.j.versluis@rocrail.net <>
2013-03-10 21:51:58 +0100

    bidib: fix for DCC accessory command

--- rocdigs/impl/bidib.c
+++ rocdigs/impl/bidib.c
@@ -518,7 +518,7 @@ static iONode __translate( iOBiDiB inst, iONode node ) {
 
           msgdata[0] = (addr-1) % 256;
           msgdata[1] = (addr-1) / 256;
-          msgdata[2] = 0x60 + (StrOp.equals(wSwitch.turnout, wSwitch.getcmd(node)) ? 0x20:0x00);
+          msgdata[2] = 0x60 + (StrOp.equals(wSwitch.turnout, wSwitch.getcmd(node)) ? 0x60:0x61);
           /* Schaltzeit
               Wert  Bedeutung
               1..63     Schaltzeit = Wert * 10ms
