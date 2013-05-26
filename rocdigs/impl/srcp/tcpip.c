abd5119b9b41d554e14f69455e8f32030e7f5af7
r.j.versluis@rocrail.net <>
2013-05-26 10:47:55 +0200

    srcp: sublib support (WIP)

---
+++ rocdigs/impl/srcp/tcpip.c
@@ -0,0 +1,60 @@
+/*
+ Rocrail - Model Railroad Software
+
+ Copyright (C) 2002-2013 Rob Versluis, Rocrail.net
+
+ Without an official permission commercial use is not permitted.
+ Forking this project is not permitted.
+
+ This program is free software; you can redistribute it and/or
+ modify it under the terms of the GNU General Public License
+ as published by the Free Software Foundation; either version 2
+ of the License, or (at your option) any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with this program; if not, write to the Free Software
+ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+*/
+
+
+#include "rocdigs/impl/srcp_impl.h"
+
+#include "rocdigs/impl/srcp/tcpip.h"
+
+
+Boolean tcpipInit( obj inst ) {
+  return False;
+}
+
+
+Boolean tcpipConnect( obj inst ) {
+  return False;
+}
+
+
+void tcpipDisconnect( obj inst ) {
+
+}
+
+
+int tcpipRead ( obj inst, char *cmd ) {
+  return 0;
+}
+
+
+Boolean tcpipWrite( obj inst, char *cmd ) {
+  return False;
+}
+
+
+Boolean tcpipAvailable( obj inst ) {
+  return False;
+}
+
+
+
