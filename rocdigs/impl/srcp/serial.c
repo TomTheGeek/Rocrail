4a7ed591c58bfcf918ece4263a05a84b53dc6a69
r.j.versluis@rocrail.net <>
2013-05-27 09:04:28 +0200

    srcp: serial sublib

--- rocdigs/impl/srcp/serial.c
+++ rocdigs/impl/srcp/serial.c
@@ -34,27 +34,90 @@
 Boolean serialInit( obj inst ) {
   iOSRCPData data = Data(inst);
   TraceOp.trc( name, TRCLEVEL_INFO, __LINE__, 9999, "  device    : %s", wDigInt.getdevice(data->ini) );
-  return False;
+
+  data->serial = SerialOp.inst( wDigInt.getdevice( data->ini ) );
+
+  SerialOp.setFlow( data->serial, none );
+
+  SerialOp.setLine( data->serial, wDigInt.getbps( data->ini ), 8, 1, none, wDigInt.isrtsdisabled( data->ini ) );
+  SerialOp.setTimeout( data->serial, wDigInt.gettimeout( data->ini ), wDigInt.gettimeout( data->ini ) );
+  return True;
 }
 
 
 int serialConnect( obj inst, Boolean info ) {
+  iOSRCPData data = Data(inst);
+  TraceOp.trc( name, TRCLEVEL_INFO, __LINE__, 9999, "connecting to [%s]", wDigInt.getdevice( data->ini ) );
+
+  if( data->serial != NULL && SerialOp.open( data->serial ) ) {
+    return SRCPCONNECT_OK;
+  }
   return SRCPCONNECT_ERROR;
 }
 
 
 void serialDisconnect( obj inst, Boolean info ) {
-
+  iOSRCPData data = Data(inst);
+  TraceOp.trc( name, TRCLEVEL_INFO, __LINE__, 9999, "disconnecting from [%s]", wDigInt.getdevice( data->ini ) );
+  if( data->serial != NULL ) {
+    SerialOp.base.del( data->serial );
+    data->serial = NULL;
+  }
 }
 
 
 int serialRead ( obj inst, char *cmd, Boolean info ) {
-  return 0;
+  iOSRCPData data = Data(inst);
+  int idx = 0;
+  cmd[idx] = '\0';
+
+  while( data->run && data->serial != NULL ) {
+    if( !SerialOp.read( data->serial, &cmd[idx], 1 ) ) {
+      return 0;
+    }
+    if( cmd[idx] == '\n' ) {
+      break;
+    }
+    idx++;
+  };
+
+  return StrOp.len(cmd);
 }
 
 
 int serialWrite( obj inst, const char *cmd, char* rsp, Boolean info ) {
-  return 0;
+  iOSRCPData data = Data(inst);
+  int retstate = 0;
+  Boolean rc = False;
+
+  if( data->serial == NULL ) {
+    return retstate;
+  }
+
+  rc = SerialOp.write( data->serial, cmd, StrOp.len(cmd) );
+
+  if(rc) {
+    char inbuf[1024] = {'\0'};
+    char szResponse[1024] = {'\0'};
+
+    if( serialRead(inst, inbuf, info) > 0 ) {
+      StrOp.replaceAll(inbuf, '\n', ' ');
+      TraceOp.trc( name, TRCLEVEL_MONITOR, __LINE__, 9999, "srcp response: %s",inbuf);
+
+      /* Scan for SM return? */
+      MemOp.set(szResponse,0,900);
+      sscanf(inbuf,"%*s %d %900c",&retstate,szResponse);
+
+      data->state = (SRCP_OK(rc)?SRCP_STATE_OK:SRCP_STATE_ERROR);
+
+      if( rsp != NULL )
+        StrOp.copy( rsp, szResponse );
+
+    }
+  }
+
+
+  return retstate;
 }
 
 
