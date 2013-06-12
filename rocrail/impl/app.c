6cc7c7a6ea0f76e20cbc743998b4f6b63673fd6a
rob <>
2013-06-12 09:25:46 +0200

    rocrail: command line help extended with the stress option

--- rocrail/impl/app.c
+++ rocrail/impl/app.c
@@ -437,6 +437,7 @@ static __help( void ) {
   TraceOp.println( "-x [planfile]            | Ini file. [%s].", wRocRail.getplanfile(NULL) );
   TraceOp.println( "-f                       | Init field." );
   TraceOp.println( "-nodevcheck              | Disable check for serial devices at startup." );
+  TraceOp.println( "-stress                  | Enable the stress runner for testing communication." );
   TraceOp.println( "-------------------------+--------------------------------------------"  );
   TraceOp.println( "-installservice          | Install Rocrail as Windows service." );
   TraceOp.println( "-deleteservice           | Uninstall Rocrail as Windows service." );
