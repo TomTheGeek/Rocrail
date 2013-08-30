f9aeced977c5eaad42cfb6f739890f0ec026d063
r.j.versluis@rocrail.net <>
2013-08-30 22:04:36 +0200

    trace correction

--- rocrail/impl/block.c
+++ rocrail/impl/block.c
@@ -1906,8 +1906,9 @@ static Boolean _unLock( iIBlockBase inst, const char* id, const char* routeId ) 
 
       if( data->locId == NULL || StrOp.len(data->locId) == 0 || StrOp.equals( id, data->locId ) || StrOp.equals( id, "*" ) ) {
         iOLocation location = ModelOp.getBlockLocation(AppOp.getModel(), data->id );
-        if( StrOp.equals( id, "*" ) ) {
-          TraceOp.trc( name, TRCLEVEL_WARNING, __LINE__, 9999, "block [%s] is unlocked by [%s]", data->id, id );
+        if( data->locId != NULL && StrOp.equals( id, "*" ) ) {
+          TraceOp.trc( name, TRCLEVEL_WARNING, __LINE__, 9999,
+              "block [%s] is unlocked by [%s]; zomby loco is [%s]", data->id, id, data->locId );
         }
 
         if( data->byRouteId != NULL && routeId != NULL && !StrOp.equals(data->byRouteId, routeId) ) {
