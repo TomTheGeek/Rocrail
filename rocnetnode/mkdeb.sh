9ecb6e226d051ae30f72ffc993524a1d6a0debcf
r.j.versluis@rocrail.net <>
2013-09-08 21:23:29 +0200

    rocnetnode: package scripts (wip)

---
+++ rocnetnode/mkdeb.sh
@@ -0,0 +1,61 @@
+#!/bin/sh
+
+
+DIST=$1
+ARCH=$2
+
+if [ !  $1 ]; then
+  echo "usage  : mkdeb.sh dist arch"
+  echo "example: mkdeb.sh raspbian armhf"
+  exit $?
+fi
+
+if [ ! $2 ]; then
+  ARCH="armhf"
+fi
+
+echo "Getting Bazaar revision number..."
+if which bzr > /dev/null
+then
+  BAZAARREV=`bzr revno .`
+  echo "    Revision number is $BAZAARREV"
+  echo ""
+else
+  BAZAARREV="user"
+  echo "    Revision number not found or Bazaar not installed, using \"user\""
+  echo ""
+fi
+
+sed s/\<BZR\>/$BAZAARREV/ < .package/control.template > ./package/control.tmp
+sed s/\<ARCH\>/$ARCH/ < ./package/control.tmp > ./package/control
+rm ./package/control.tmp
+
+
+if [ ! -e ./raspbian ] ; then
+	mkdir ./raspbian
+fi
+cd ./raspbian
+rm -Rf *.deb
+mkdir -p debian/DEBIAN
+mkdir -p debian/opt/rocnetnode
+mkdir -p debian/etc/init.d
+
+cp ./package/control debian/DEBIAN/control
+
+cp ../unxbin/rocnetnode debian/opt/rocnetnode
+cp ../unxbin/dcc232.so debian/opt/rocrail
+
+strip debian/opt/rocnetnode/rocnetnode
+strip debian/opt/rocnetnode/*.so
+
+cp ./package/rocnetnode.sh debian/opt/rocnetnode
+cp ./package/rocnetnoded debian/etc/init.d
+chmod +x debian/opt/rocnetnode/*.sh
+cp ./package/rocnetnode.ini debian/opt/rocnetnode
+
+cp -R ../COPYING debian/opt/rocnetnode
+
+fakeroot dpkg-deb --build debian
+mv debian.deb rocnetnode-$BAZAARREV-$DIST-$ARCH.deb
+rm -Rf debian
+cd ..
