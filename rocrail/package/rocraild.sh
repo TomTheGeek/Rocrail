6e5c3b197d7f6f086790220cebb615ef482701f2
Rob Versluis <r.j.versluis@rocrail.net>
2014-05-05 07:31:54 +0200

    runlevels added http://forum.rocrail.net/viewtopic.php?f=94&t=7613

--- rocrail/package/rocraild.sh
+++ rocrail/package/rocraild.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 cd /opt/rocrail/
 rm -f nohup.out
-nohup ./rocrail -l /opt/rocrail&
+#nohup ./rocrail -l /opt/rocrail&
+nohup ./rocrail -l /opt/rocrail > nohup.out 2>&1 &
 echo "$!" > /tmp/rocraild.pid
