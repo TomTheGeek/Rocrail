2c66cf3d3897d17bd02ece6c4d55062d6d9cb8b2
Rob Versluis <r.j.versluis@rocrail.net>
2014-05-18 14:12:41 +0200

    copyright update

--- rocdigs/impl/ucons88.c
+++ rocdigs/impl/ucons88.c
@@ -1,18 +1,23 @@
-/** ------------------------------------------------------------
-  * A U T O   G E N E R A T E D  (First time only!)
-  * Generator: Rocs ogen (build Sep 26 2009 06:34:50)
-  * Module: RocDigs
-  * XML: $Source: /cvsroot/rojav/rocdigs/rocdigs.xml,v $
-  * XML: $Revision: 1.14 $
-  * Object: uConS88
-  * Date: Sun May  4 17:24:26 2014
-  * ------------------------------------------------------------
-  * $Source$
-  * $Author$
-  * $Date$
-  * $Revision$
-  * $Name$
-  */
+/*
+ Rocrail - Model Railroad Software
+
+ Copyright (C) 2002-2014 Rob Versluis & Hagen Burchardt, Rocrail.net
+
+ This program is free software; you can redistribute it and/or
+ modify it under the terms of the GNU General Public License
+ as published by the Free Software Foundation; either version 2
+ of the License, or (at your option) any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with this program; if not, write to the Free Software
+ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+*/
+
 
 #include "rocdigs/impl/ucons88_impl.h"
 
