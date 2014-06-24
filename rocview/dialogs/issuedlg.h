00fc0c714963f7de7b2a8131719e394776085a43
Rob Versluis <r.j.versluis@rocrail.net>
2014-06-24 20:52:40 +0200

    replaced \r with \n in derived class files

--- rocview/dialogs/issuedlg.h
+++ rocview/dialogs/issuedlg.h
@@ -1,1 +1,50 @@
-/* Rocrail - Model Railroad Software Copyright (C) 2002-2014 Rob Versluis, Rocrail.net  This program is free software; you can redistribute it and/or as published by the Free Software Foundation; either version 2 modify it under the terms of the GNU General Public License of the License, or (at your option) any later version. This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details. You should have received a copy of the GNU General Public License along with this program; if not, write to the Free Software Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.*/#ifndef __issuedlg__#define __issuedlg__#include "issuedlggen.h"#include "rocs/public/node.h"//// end generated include/** Implementing IssueDlgGen */class IssueDlg : public IssueDlgGen{  iONode m_Issue;	protected:		// Handlers for IssueDlgGen events.		void onCancel( wxCommandEvent& event );		void onOK( wxCommandEvent& event );		void onHelp( wxCommandEvent& event );	public:		/** Constructor */		IssueDlg( wxWindow* parent );		iONode getIssue() {return m_Issue;}	//// end generated class members	};#endif // __issuedlg__
+/*
+ Rocrail - Model Railroad Software
+
+ Copyright (C) 2002-2014 Rob Versluis, Rocrail.net
+
+ 
+
+
+ This program is free software; you can redistribute it and/or
+ as published by the Free Software Foundation; either version 2
+ modify it under the terms of the GNU General Public License
+ of the License, or (at your option) any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with this program; if not, write to the Free Software
+ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+*/
+
+#ifndef __issuedlg__
+#define __issuedlg__
+
+#include "issuedlggen.h"
+
+#include "rocs/public/node.h"
+
+//// end generated include
+
+/** Implementing IssueDlgGen */
+class IssueDlg : public IssueDlgGen
+{
+  iONode m_Issue;
+	protected:
+		// Handlers for IssueDlgGen events.
+		void onCancel( wxCommandEvent& event );
+		void onOK( wxCommandEvent& event );
+		void onHelp( wxCommandEvent& event );
+	public:
+		/** Constructor */
+		IssueDlg( wxWindow* parent );
+		iONode getIssue() {return m_Issue;}
+	//// end generated class members
+	
+};
+
+#endif // __issuedlg__
