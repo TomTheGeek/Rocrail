9f612216d9272deae8d99a6c56e14282ff01ed1f
Rob Versluis <r.j.versluis@rocrail.net>
2014-09-26 17:00:23 +0200

    inter10: restored

--- rocview/dialogs/rocrailinidialog.cpp
+++ rocview/dialogs/rocrailinidialog.cpp
@@ -616,6 +616,7 @@ void RocrailIniDialog::initValues() {
   m_Lib->Append( wxString( wDigInt.got, wxConvUTF8 ) );
   m_Lib->Append( wxString( wDigInt.hsi88, wxConvUTF8 ) );
   m_Lib->Append( wxString( wDigInt.infracar, wxConvUTF8 ) );
+  m_Lib->Append( wxString( wDigInt.inter10, wxConvUTF8 ) );
   m_Lib->Append( wxString( wDigInt.loconet, wxConvUTF8 ) );
   m_Lib->Append( wxString( wDigInt.lrc135, wxConvUTF8 ) );
   m_Lib->Append( wxString( wDigInt.massoth, wxConvUTF8 ) );
